6f5a589936214f073dd47f523160a2ebc44053da
Nolan <96299649+VNolanV@users.noreply.github.com>
2025-02-26 02:58:15 -0500

    Upload of helloWorld.c file.

---
+++ helloWorld.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>	
+#include <time.h>	
+#include <stdbool.h>		
+
+int main() {
+
+    //Simple Introduction Message
+    printf("\n\n--------\nSimple Encrpytion v0.1\n-------\n\n");
+    printf("Please");
+    
+
+
+    return 0;
+}
